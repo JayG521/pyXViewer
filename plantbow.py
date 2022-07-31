8af09fbcc6b86c8f1c76fdec85df967a2fa24b85
JayG521 <104608922+JayG521@users.noreply.github.com>
2022-07-31 19:24:02 +1200

    gui #005

--- plantbow.py
+++ plantbow.py
@@ -9,33 +9,38 @@ from tkinter import PhotoImage, ttk
 root = tkinter.Tk()
 mainApp = ttk.Frame(root, padding=10)
 mainApp.grid()
-mainApp.master.maxsize(630, 800)
-mainApp.master.minsize(630, 510)
+mainApp.master.maxsize(680, 800)
+mainApp.master.minsize(680, 510)
 mainApp.master.resizable(0, 1)
 
 # sub section 1
 # 
 canon = ttk.Frame(mainApp, relief='groove', borderwidth=60, padding=6)
-canon.grid(column=0, row= 0)
+canon.grid(column=0, row=0, sticky='w')
 # inital parts
 ttk.Label(canon, text="pyXViewer   ").grid(column=0, row=0, padx=5, sticky='w')
-ttk.Label(canon, text="v0.8").grid(column=0, row=1)
-# important drop box
-picker = ttk.Combobox(canon, textvariable="elements", 
-values=('shade', 'zone', 'price', '[ light ]')).grid(column=1, row=2, pady=30)
+ttk.Label(canon, text="v0.8").grid(column=0, row=1, padx=59)
 
 # sub section 2
 #
 fuji = ttk.Frame(mainApp, relief='groove', borderwidth=6, padding=20)
 fuji.grid(column=0, row=1, sticky='w')
-cola1 = ttk.Label(fuji, text='test').grid(column=0, row=0)
-cola2 = ttk.Label(fuji, text='test2').grid(column=1, row=0, padx=9)
-cola3 = ttk.Radiobutton(fuji, text='botanical').grid(column=2, row=0, padx=40)
+cola1 = ttk.Label(fuji, text='element 1').grid(column=0, row=0, sticky='w')
+cola2 = ttk.Label(fuji, text='element 2').grid(column=2, row=0, pady =6)
+cola1 = ttk.Label(fuji, text='element 3').grid(column=4, row=0, padx =6)
+# important drop box
+picker1 = ttk.Combobox(fuji, textvariable="elements", 
+   values=('[none]', 'shade', 'zone', 'price', 'light')).grid(column=1, row=0, pady=30)
+picker2 = ttk.Combobox(fuji, textvariable="elements", 
+   values=('[none]',' shade', 'zone', 'price', 'light')).grid(column=3, row=0, pady=10)
+picker3 = ttk.Combobox(fuji, textvariable="elements", 
+   values=('[none]', 'shade', 'zone', 'price', 'light')).grid(column=5, row=0, pady=30)
+
 
 # sub section 3
 #
 epson = ttk.Frame(mainApp, relief='groove', borderwidth=6, padding=20)
-epson.grid(column=0, row= 2, sticky='e')
+epson.grid(column=0, row= 2, sticky='w')
 # main button
 # generates new window
 ttk.Button(epson, text='generate').grid(column=1, row=5, pady=33, sticky='s')
