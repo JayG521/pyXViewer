56818212eac3d43a702abacf7cea975f1db8d97b
JayG521 <104608922+JayG521@users.noreply.github.com>
2022-09-24 13:24:06 +1200

    feat: Upgrade sync

--- plantbow.py
+++ plantbow.py
@@ -2,36 +2,79 @@
 # with tkinter
 #
 #
-
+# import tools
 import plantreader
 import tkinter
 from tkinter import PhotoImage, Toplevel, ttk
 
+
+# multiple inital variables
+#
+i_list = range(0, 35, 1)
+
+tags_drop = ['[none]', 'zone', 'price', 'light']
+rush = 'test top anchor'
+granTur = 'test bottom achor'
+
+catEl1 = ['sample']
+catEl2 = ['sample']
+catEl3 = ['sample']
+for catfill in i_list:
+    catfill = ['sample']
+    catEl1 = catEl1 + catfill
+    catEl2 = catEl2 + catfill
+    catEl3 = catEl3 + catfill
+
+
+# gen new window
+# expApp frame news to hold dependant variables
+#
+def lazysummer():
+   wNew = Toplevel()
+   expApp = ttk.Frame(wNew, padding=10)
+   expApp.master.minsize(700, 785)
+   ttk.Label(wNew, text=rush).grid(column=0, row=0, padx=9, pady=9)
+   ttk.Label(wNew, text=granTur).grid(column=8, row=40, padx=79, pady=1)
+
+   # variable lists
+   # needs re work 
+   # 
+   # choosen for first catagory (element)
+   if picker1.get() == '[none]':
+      for tyty in i_list:
+         catEl1[tyty] = ''
+   elif picker1.get() == 'zone':
+      for tyty in i_list:
+         catEl1[tyty] = plantreader.plantZone[tyty]
+   elif picker1.get() == 'price':
+      for tyty in i_list:
+         catEl1[tyty] = plantreader.plantPrice[tyty]
+   elif picker1.get() == 'light':
+      for tyty in i_list:
+         catEl1[tyty] = plantreader.plantLight[tyty]
+   else:
+      for tyty in i_list:
+         catEl1[tyty] = ''
+
+
+   for gg in i_list:
+      # column 1 (name)
+      ttk.Label(wNew, text=plantreader.plantNames[gg]).grid(column=1, row=2+gg, padx=80)
+      # column 2 (elemnt 1)
+      ttk.Label(wNew, text=catEl1[gg]).grid(column=2, row=2+gg, padx=20)
+      # column 3 (elemnt 2)
+      ttk.Label(wNew, text=catEl2[gg]).grid(column=3, row=2+gg, padx=20)
+      # column 3 (elemnt 3)
+      ttk.Label(wNew, text=catEl3[gg]).grid(column=4, row=2+gg, padx=20)
+
+
+# Main Window App
 root = tkinter.Tk()
 mainApp = ttk.Frame(root, padding=10)
 mainApp.grid()
 mainApp.master.maxsize(680, 800)
 mainApp.master.minsize(680, 510)
 mainApp.master.resizable(0, 1)
-
-
-tags_drop = ['[none]', 'shade', 'zone', 'price', 'light']
-rush = 'test string'
-i_list = range(0, 15, 1)
-
-
-# expApp frame news to hold dependant 
-#
-def lazysummer():
-   wNew = Toplevel()
-   expApp = ttk.Frame(wNew, padding=10)
-   expApp.master.minsize(700, 700)
-   ttk.Label(wNew, text=rush).grid(column=0, row=1, padx=259, pady=188)
-   
-   for gg in i_list:
-      ttk.Label(wNew, text='test me').grid(column=0, row=1+gg, padx=130)
-      ttk.Label(wNew, text=' collume 2222').grid(column=1, row=1+gg, padx=90)
-
 
 # sub section 1
 # 
@@ -65,8 +108,7 @@ epson = ttk.Frame(mainApp, relief='groove', borderwidth=6, padding=20)
 epson.grid(column=0, row= 2, sticky='w')
 # main button
 # generates new window
-ttk.Button(epson, text='generate', command=testgetwidget).grid(column=1, row=5, pady=33, sticky='s')
-
+ttk.Button(epson, text='generate', command=lazysummer).grid(column=1, row=5, pady=33, sticky='s')
 
 # window dressing
 # image
